91e82684ff46d7bc3a4156dddb63519557efc300
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2019-04-06 14:01:09 -0400

    wip

---
+++ prep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check who am i
+if [ "$USER" != "root" ]
+then
+	echo "Only root can run this script"
+	exit 1
+fi
+
+apt-get install debootstrap
+
+debootstrap	\
+	--verbose 
+	--variant=minbase
+	--arch=amd64
+	stretch ./node 
+	http://ftp.pl.debian.org/debian
+
+
