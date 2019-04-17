fb008e08a8ff31362ea489b041959e446f2b0d9d
Tomasz Jaworski <tjaworski@iis.p.lodz.pl>
2019-04-18 00:47:49 +0200

    Network configuration fixups

--- service_tftp/make.sh
+++ service_tftp/make.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+ln  ../nfsroot/boot/vmlinuz-4.9.0-8-amd64 vmlinuz-4.9.0-8-amd64
+ln  ../nfsroot/boot/initrd.img-4.9.0-8-amd64 initrd.img-4.9.0-8-amd64
+
 docker build --tag=dante_tftp .
 
+unlink vmlinuz-4.9.0-8-amd64
+unlink initrd.img-4.9.0-8-amd64
