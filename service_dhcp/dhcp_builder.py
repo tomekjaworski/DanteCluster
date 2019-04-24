5c2c534e878d73eef7349574464022015701ba87
Tomasz Jaworski <tjaworski@tjaworski.pl>
2019-04-24 14:39:58 +0200

    Automated generation of WOL (wake up on LAN) bash scripts

--- service_dhcp/dhcp_builder.py
+++ service_dhcp/dhcp_builder.py
@@ -1,16 +1,27 @@
 import jinja2
 import json
 import random
+import os
 
 
 def ReadFile(fileName: str) -> str:
     with open(fileName, "rt") as f:
         return f.read()
 
+def add_exec_attr(fname: str):
+    mode = os.stat(fname).st_mode
+    mode |= 0o111
+    os.chmod(fname, mode)
+
+    
+
+
 if __name__ == "__main__":
 
 
-    template = jinja2.Template(ReadFile("dhcpd.conf.j2template"))
+    #
+    # Read hardware description file
+    #
     hardware = json.loads(ReadFile("machines.json"))
 
 
@@ -20,10 +31,36 @@ if __name__ == "__main__":
 
         print("Machine %s -> ip=%s" % (machine['hardware'], machine['ip']))
 
+    #
+    # Create DHCP server configuration file by template (dhcpd.conf)
+    #
+
+    template = jinja2.Template(ReadFile("dhcpd.conf.j2template"))
     output = template.render(machines=hardware["machines"])
 
     with open("dhcpd.conf", "wt") as f:
         f.write(output)
+
+    #
+    # Prepare etherware scripts for all nodes
+    #
+
+    for machine in hardware["machines"]:
+        content = f"""#!/bin/bash
+#
+# Wake UP host {machine['ip']} with hardware addres {machine['hardware']}
+#
+
+etherwake -i inner {machine['hardware']}
+echo Done.
+
+"""
+        fname = "wake_" + machine["hostname"] + ".sh"
+        with open(fname, "wt") as f:
+            f.write(content)
+
+        add_exec_attr(fname)
+
 
     print("Done.")
 
