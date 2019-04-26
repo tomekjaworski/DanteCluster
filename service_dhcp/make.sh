2534964295544af9953f750b9b1fb3768c200e4e
Tomasz Jaworski <tjaworski@tjaworski.pl>
2019-04-26 17:47:02 +0200

    Minor script changes

--- service_dhcp/make.sh
+++ service_dhcp/make.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 docker build --tag=dante_dhcp .
+readonly DANGLING=$(docker images --format {{.ID}} --filter dangling=true)
+[[ -z $DANGLING ]] && echo "No dangling images found." || docker rmi $DANGLING
 
