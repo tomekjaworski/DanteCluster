d7c1aa3eecc9b58a815a8b214d2ecbabee8218a3
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2019-04-27 22:41:43 +0200

    Misc

--- Ctrl/Ctrl/Program.cs
+++ Ctrl/Ctrl/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Net.NetworkInformation;
 using System.Reflection.PortableExecutable;
 using System.Threading;
@@ -11,6 +12,25 @@ using Newtonsoft.Json.Linq;
 
 namespace Ctrl
 {
+
+    public class NodePingStatus {
+        public IPAddress IP { get; private set; }
+
+        public NodePingStatus(IPAddress ip)
+        {
+            this.IP = ip;
+        }
+
+        public void SetICMPStatus(IPStatus replyStatus)
+        {
+            lock (this)
+            {
+                if (replyStatus == IPStatus.Success)
+                    this.responseseInARow++;
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -24,6 +44,8 @@ namespace Ctrl
     public class ClusterController
     {
         private ClusterConfiguration config;
+        private CancellationTokenSource cts;
+        private Dictionary<IPAddress, NodePingStatus> pingStatuses;
 
         public ClusterController(string configFileName)
         {
@@ -35,22 +57,35 @@ namespace Ctrl
             settings.Formatting = Formatting.Indented;
 
             this.config = JsonConvert.DeserializeObject<ClusterConfiguration>(json, settings);
+
+            this.cts = new CancellationTokenSource();
+            this.pingStatuses = new Dictionary<IPAddress, NodePingStatus>();
+            foreach (Node node in this.config.Nodes)
+                this.pingStatuses.Add(node.IP, new NodePingStatus(node.IP));
         }
 
         private async Task Pinger()
         {
             List<Task<PingReply>> tasks = new List<Task<PingReply>>();
-            foreach (Node node in this.config.Nodes)
+
+            while (!this.cts.IsCancellationRequested)
             {
-                Ping ping = new Ping();
-                Task<PingReply> pr = ping.SendPingAsync(node.IP, 2000);
-                tasks.Add(pr);
+                tasks.Clear();
+                foreach (Node node in this.config.Nodes)
+                {
+                    Ping ping = new Ping();
+                    Task<PingReply> pr = ping.SendPingAsync(node.IP, 2000);
+                    tasks.Add(pr);
+                }
+
+                PingReply[] replies = await Task.WhenAll(tasks);
+
+                foreach (PingReply reply in replies)
+                {
+                    this.pingStatuses[reply.Address].SetICMPStatus(reply.Status);
+                }
+
             }
-
-      
-
-            PingReply[] replies = await Task.WhenAll(tasks);
-
         }
 
         public void Run()
@@ -60,9 +95,7 @@ namespace Ctrl
 
 
             while (true)
-            {
                 Thread.Sleep(1000);
-            }
         }
     }
 }
