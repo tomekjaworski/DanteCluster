39fe506762bc8c5abdf9f82da7073d485dfc6543
Tomasz Jaworski <tjaworski@tjaworski.pl>
2019-05-12 11:06:58 +0200

    Docker configuration fixups for TFTP and DHCP services

--- make-tftp.sh
+++ make-tftp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 readonly TFTP_FILES_VOL="tftp_files"
-readonly FILEPATH="../test"
+readonly FILEPATH="test"
 
 echo "Making docker volume with tftp files"
-docker run --rm -v $(pwd)/$FILEPATH:/files0 --mount source=$TFTP_FILES_VOL,destination=/files1 alpine:3.9 /bin/sh -c "cp /files0/* /files1"
+docker run --rm -v $(pwd)/$FILEPATH:/files0 --mount source=$TFTP_FILES_VOL,destination=/files1 alpine:3.9 cp -r /files0/ /files1 1>/dev/null
 
 echo "Building docker tftp server."
-docker build -t tftp -f Dockerfile . 1>/dev/null
+docker build -t tftp -f service_tftp/Dockerfile . 1>/dev/null
 
 echo "Remove dangling docker images."
 docker rmi $(docker images -f "dangling=true" -q) 1>/dev/null
--- service_tftp/runme.sh
+++ service_tftp/runme.sh
@@ -3,9 +3,22 @@
 readonly TFTP_LOG_VOL="tftp_log_vol"
 readonly TFTP_FILES_VOL="tftp_files"
 
+function stop_and_remove_container() {
+	if [[ ! -z "$(docker ps | grep $1)" ]]; then
+		echo "Removing old $1 container..."
+		docker stop $1
+		docker rm $1
+	fi
+}
+
+stop_and_remove_container "tftp_server"
+
 docker run -d \
     --name tftp_server \
+    --network=host \
     --mount source="$TFTP_LOG_VOL",destination=/logs \
-    --mount source="$TFTP_FILES_VOL",destination=/tftpboot,readonly \
+    --mount source="$TFTP_FILES_VOL",destination=/tftpboot/kern,readonly \
     tftp
+
+sleep 1
 docker ps -a --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Names}}\t{{.Ports}}\t{{.Mounts}}" | grep "tftp_server"
